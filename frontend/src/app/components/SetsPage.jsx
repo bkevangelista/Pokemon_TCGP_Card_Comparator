a21c79826f82e7c4713ba0e5c42339d7a1ebfbbc
Branden Evangelista <evangelistabranden@gmail.com>
2025-08-13 17:16:13 -0700

    feat(tcgpSet): update sets page to pull up all cards from a selected set

--- frontend/src/app/components/SetsPage.jsx
+++ frontend/src/app/components/SetsPage.jsx
@@ -1,24 +1,89 @@
 "use client";
 
+import apiRoutes from "@/app/utils/apiRoutes"
+
+import Image from "next/image";
 import { useState, useEffect } from "react";
+import Select from "react-select"
 
 export default function SetPage() {
-  const [tcgpSets, setTCGPSets] = useState([]);
+    const [tcgpSets, setTCGPSets] = useState([]);
+    const [selectedSet, setSelectedSet] = useState("");
+    const [cards, setCards] = useState([]);
+    const [loadingSets, setLoadingSets] = useState(false);
+    const [loadingCards, setLoadingCards] = useState(false);
 
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/external/tcg/getSets`)
-      .then(res => res.json())
-      .then(data => setTCGPSets(data.tcgSets));
-  }, []);
+    useEffect(() => {
+        const fetchSets = async () => {
+            setLoadingSets(true);
+            try {
+                const res = await fetch(apiRoutes.getSets);
+                const data = await res.json();
 
-  return (
-    <div>
-      <h1>All Sets</h1>
-      <ul>
-        {tcgpSets.map((set) => (
-          <li key={set.set_id}>{`${set.set_name} - ${set.set_id}`}</li>
-        ))}
-      </ul>
-    </div>
-  );
+                const sets = data.tcgSets;
+
+                const setOptions = sets.map(s => ({
+                   value: s.set_id,
+                   label: s.set_name
+                }));
+
+                setTCGPSets(setOptions);
+            } catch (err) {
+                console.error("Error fetching set names: ", err);
+            } finally {
+                setLoadingSets(false);
+            }
+
+        }
+        fetchSets();
+    }, []);
+
+    const fetchCards = async (setId) => {
+        setLoadingCards(true);
+        try {
+            const res = await fetch(apiRoutes.getCards(setId));
+            const data = await res.json();
+            setCards(data.cards)
+        } catch (err) {
+            console.error(`Error fetching cards of set id ${setId}: `, err);
+        } finally {
+            setLoadingCards(false);
+        }
+    }
+
+    const handleSetChange = (e) => {
+        const setId = e.value;
+        setSelectedSet(setId);
+        if (setId) {
+            fetchCards(setId);
+        } else {
+            setCards([]);
+        }
+    }
+
+    return (
+        <div>
+            <h1>Select a Pokémon TCGP Set</h1>
+            {loadingSets ? (
+                <p>Loading Sets</p>
+            ) : (
+                <Select options={tcgpSets} onChange={handleSetChange} />
+            )}
+
+            {loadingCards ? (
+                <p>Loading Cards</p>
+            ) : (
+                selectedSet && (
+                    <ul>
+                        {cards.map((card) => (
+                            <li key={card.id}>
+                                <Image src={`${card.image}/low.png`} alt={card.image} />
+                                {card.name} - {card.rarity}
+                            </li>
+                        ))}
+                    </ul>
+                )
+            )}
+        </div>
+    );
 }
---
+++ frontend/src/app/utils/apiRoutes.js
@@ -0,0 +1,9 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+const apiRoutes = {
+    getSets: `${API_URL}/external/tcg/getSets`,
+    getCards: (setId) =>
+        `${API_URL}/external/tcg/getCards?set_id=${setId}`,
+};
+
+export default apiRoutes;
