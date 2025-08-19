968a4ea78b3e9cd76bd93238008d4ccf90341fe3
Branden Evangelista <evangelistabranden@gmail.com>
2025-08-19 15:54:15 -0700

    feat(backend): add endpoint to compare cards of both users

---
+++ backend/src/constants/CardRarity.py
@@ -0,0 +1,13 @@
+class CardRarity:
+    CARD_RARITY = {
+        "One Diamond": 1,
+        "Two Diamond": 2,
+        "Three Diamond": 3,
+        "Four Diamond": 4,
+        "One Star": 5,
+        "Two Star": 6,
+        "One Shiny": 7,
+        "Two Shiny": 8,
+        "Three Star": 9,
+        "Crown": 10,
+    }
--- backend/src/models/UserCard.py
+++ backend/src/models/UserCard.py
@@ -5,3 +5,6 @@ class UserCard(BaseModel):
     card_id: str
     set_id: str
     no_owned: int
+    image: str | None = None
+    rarity: str | None = None
+    name: str | None = None
